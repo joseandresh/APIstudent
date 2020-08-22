5ab53bb923f4f8e9adc1f30d7046adbcd9f19b17
joseandresh <a2019112027@estudiantes.upsa.edu.bo>
2020-08-22 13:43:28 -0400

    Se creo el API Web de estudiante.

---
+++ APIstudent/APIstudent/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIStudent.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+		public System.Data.Entity.DbSet<APIstudent.Models.Student> Students { get; set; }
+	}
+}
